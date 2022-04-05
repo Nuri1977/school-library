0c3af183dc91869e522f9b9f8b1594dd9cf1b88e
Nuri1977 <nurilacka@gmail.com>
2022-04-05 09:37:49 +0200

    Create  many-to-many relationship between Person and Book using the intermediate class Rental

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
 require './decorator'
+require './rental'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -10,6 +11,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   def can_use_services?
